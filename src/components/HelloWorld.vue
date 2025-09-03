979aac4fbc65c624d666a7aa6864ad7439eeebc6
yukakikuchi-mippy <ykikuchi@dl-techx-xedge.net>
2025-09-03 11:29:23 +0900

    資源のデプロイ

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,7 +10,7 @@
         />
       </v-col>
 
-      <v-col class="mb-4">
+      <v-col class="mb-5">
         <h1 class="display-2 font-weight-bold mb-3">
           Welcome to Vuetify
         </h1>
